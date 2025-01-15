ec3ed1507bb8c0eaeef0fc3035ffc4f692c4ead8
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-01-15 10:55:52 +0900

    feat: Cropping detected faces from frames of live camera footage in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,7 @@ class _MyHomePageState extends State<MyHomePage> {
     print("fl=" + faces.length.toString());
 
     //TODO perform face recognition on detected faces
-    // performFaceRecognition(faces);
+    performFaceRecognition(faces);
 
     setState(() {
       _scanResults = faces;
@@ -112,32 +112,33 @@ class _MyHomePageState extends State<MyHomePage> {
   bool register = false;
 
   // TODO perform Face Recognition
-  // performFaceRecognition(List<Face> faces) async {
-  //   recognitions.clear();
-  //
-  //   //TODO convert CameraImage to Image and rotate it so that our frame will be in a portrait
-  //   image = convertYUV420ToImage(frame!);
-  //   image =img.copyRotate(image!, angle: camDirec == CameraLensDirection.front?270:90);
-  //
-  //   for (Face face in faces) {
-  //     Rect faceRect = face.boundingBox;
-  //     //TODO crop face
-  //     img.Image croppedFace = img.copyCrop(image!, x:faceRect.left.toInt(),y:faceRect.top.toInt(),width:faceRect.width.toInt(),height:faceRect.height.toInt());
-  //
-  //     //TODO pass cropped face to face recognition model
-  //
-  //
-  //     //TODO show face registration dialogue
-  //
-  //
-  //   }
-  //
-  //   setState(() {
-  //     isBusy  = false;
-  //     _scanResults = recognitions;
-  //   });
-  //
-  // }
+  performFaceRecognition(List<Face> faces) async {
+    recognitions.clear();
+
+    //TODO convert CameraImage to Image and rotate it so that our frame will be in a portrait
+    image = convertYUV420ToImage(frame!);
+    image = img.copyRotate(image!,
+        angle: camDirec == CameraLensDirection.front ? 270 : 90);
+
+    for (Face face in faces) {
+      Rect faceRect = face.boundingBox;
+      //TODO crop face
+      img.Image croppedFace = img.copyCrop(image!,
+          x: faceRect.left.toInt(),
+          y: faceRect.top.toInt(),
+          width: faceRect.width.toInt(),
+          height: faceRect.height.toInt());
+
+      //TODO pass cropped face to face recognition model
+
+      //TODO show face registration dialogue
+    }
+
+    setState(() {
+      isBusy = false;
+      // _scanResults = recognitions;
+    });
+  }
 
   //TODO Face Registration Dialogue
   // TextEditingController textEditingController = TextEditingController();
@@ -179,50 +180,49 @@ class _MyHomePageState extends State<MyHomePage> {
   // }
 
   // TODO method to convert CameraImage to Image
-  // img.Image convertYUV420ToImage(CameraImage cameraImage) {
-  //   final width = cameraImage.width;
-  //   final height = cameraImage.height;
-  //
-  //   final yRowStride = cameraImage.planes[0].bytesPerRow;
-  //   final uvRowStride = cameraImage.planes[1].bytesPerRow;
-  //   final uvPixelStride = cameraImage.planes[1].bytesPerPixel!;
-  //
-  //   final image = img.Image(width:width, height:height);
-  //
-  //   for (var w = 0; w < width; w++) {
-  //     for (var h = 0; h < height; h++) {
-  //       final uvIndex =
-  //           uvPixelStride * (w / 2).floor() + uvRowStride * (h / 2).floor();
-  //       final index = h * width + w;
-  //       final yIndex = h * yRowStride + w;
-  //
-  //       final y = cameraImage.planes[0].bytes[yIndex];
-  //       final u = cameraImage.planes[1].bytes[uvIndex];
-  //       final v = cameraImage.planes[2].bytes[uvIndex];
-  //
-  //       image.data!.setPixelR(w, h, yuv2rgb(y, u, v));//= yuv2rgb(y, u, v);
-  //     }
-  //   }
-  //   return image;
-  // }
-  // int yuv2rgb(int y, int u, int v) {
-  //   // Convert yuv pixel to rgb
-  //   var r = (y + v * 1436 / 1024 - 179).round();
-  //   var g = (y - u * 46549 / 131072 + 44 - v * 93604 / 131072 + 91).round();
-  //   var b = (y + u * 1814 / 1024 - 227).round();
-  //
-  //   // Clipping RGB values to be inside boundaries [ 0 , 255 ]
-  //   r = r.clamp(0, 255);
-  //   g = g.clamp(0, 255);
-  //   b = b.clamp(0, 255);
-  //
-  //   return 0xff000000 |
-  //   ((b << 16) & 0xff0000) |
-  //   ((g << 8) & 0xff00) |
-  //   (r & 0xff);
-  // }
+  img.Image convertYUV420ToImage(CameraImage cameraImage) {
+    final width = cameraImage.width;
+    final height = cameraImage.height;
 
-  //TODO convert CameraImage to InputImage
+    final yRowStride = cameraImage.planes[0].bytesPerRow;
+    final uvRowStride = cameraImage.planes[1].bytesPerRow;
+    final uvPixelStride = cameraImage.planes[1].bytesPerPixel!;
+
+    final image = img.Image(width: width, height: height);
+
+    for (var w = 0; w < width; w++) {
+      for (var h = 0; h < height; h++) {
+        final uvIndex =
+            uvPixelStride * (w / 2).floor() + uvRowStride * (h / 2).floor();
+        final index = h * width + w;
+        final yIndex = h * yRowStride + w;
+
+        final y = cameraImage.planes[0].bytes[yIndex];
+        final u = cameraImage.planes[1].bytes[uvIndex];
+        final v = cameraImage.planes[2].bytes[uvIndex];
+
+        image.data!.setPixelR(w, h, yuv2rgb(y, u, v)); //= yuv2rgb(y, u, v);
+      }
+    }
+    return image;
+  }
+
+  int yuv2rgb(int y, int u, int v) {
+    // Convert yuv pixel to rgb
+    var r = (y + v * 1436 / 1024 - 179).round();
+    var g = (y - u * 46549 / 131072 + 44 - v * 93604 / 131072 + 91).round();
+    var b = (y + u * 1814 / 1024 - 227).round();
+
+    // Clipping RGB values to be inside boundaries [ 0 , 255 ]
+    r = r.clamp(0, 255);
+    g = g.clamp(0, 255);
+    b = b.clamp(0, 255);
+
+    return 0xff000000 |
+        ((b << 16) & 0xff0000) |
+        ((g << 8) & 0xff00) |
+        (r & 0xff);
+  }
 
   final _orientations = {
     DeviceOrientation.portraitUp: 0,
@@ -231,6 +231,7 @@ class _MyHomePageState extends State<MyHomePage> {
     DeviceOrientation.landscapeRight: 270,
   };
 
+  //TODO convert CameraImage to InputImage
   InputImage? getInputImage() {
     final camera =
         camDirec == CameraLensDirection.front ? cameras[1] : cameras[0];
@@ -335,73 +336,77 @@ class _MyHomePageState extends State<MyHomePage> {
       //TODO View for displaying rectangles around detected aces
       stackChildren.add(
         Positioned(
-            top: 0.0,
-            left: 0.0,
-            width: size.width,
-            height: size.height,
-            child: buildResult()),
+          top: 0.0,
+          left: 0.0,
+          width: size.width,
+          height: size.height,
+          child: buildResult(),
+        ),
       );
     }
 
     //TODO View for displaying the bar to switch camera direction or for registering faces
-    stackChildren.add(Positioned(
-      top: size.height - 140,
-      left: 0,
-      width: size.width,
-      height: 80,
-      child: Card(
-        margin: const EdgeInsets.only(left: 20, right: 20),
-        color: Colors.blue,
-        child: Center(
-          child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    IconButton(
-                      icon: const Icon(
-                        Icons.cached,
-                        color: Colors.white,
+    stackChildren.add(
+      Positioned(
+        top: size.height - 140,
+        left: 0,
+        width: size.width,
+        height: 80,
+        child: Card(
+          margin: const EdgeInsets.only(left: 20, right: 20),
+          color: Colors.blue,
+          child: Center(
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.cached,
+                          color: Colors.white,
+                        ),
+                        iconSize: 40,
+                        color: Colors.black,
+                        onPressed: () {
+                          _toggleCameraDirection();
+                        },
                       ),
-                      iconSize: 40,
-                      color: Colors.black,
-                      onPressed: () {
-                        _toggleCameraDirection();
-                      },
-                    ),
-                    Container(
-                      width: 30,
-                    ),
-                    IconButton(
-                      icon: const Icon(
-                        Icons.face_retouching_natural,
-                        color: Colors.white,
+                      Container(
+                        width: 30,
                       ),
-                      iconSize: 40,
-                      color: Colors.black,
-                      onPressed: () {},
-                    )
-                  ],
-                ),
-              ],
+                      IconButton(
+                        icon: const Icon(
+                          Icons.face_retouching_natural,
+                          color: Colors.white,
+                        ),
+                        iconSize: 40,
+                        color: Colors.black,
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
-    ));
+    );
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.black,
         body: Container(
-            margin: const EdgeInsets.only(top: 0),
-            color: Colors.black,
-            child: Stack(
-              children: stackChildren,
-            )),
+          margin: const EdgeInsets.only(top: 0),
+          color: Colors.black,
+          child: Stack(
+            children: stackChildren,
+          ),
+        ),
       ),
     );
   }
